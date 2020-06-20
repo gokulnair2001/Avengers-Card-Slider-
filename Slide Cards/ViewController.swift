ff1b0d880bbf6bc228015cafc3aa7523ce922119
“gokulnair2001” <gokulnair.2001@gmail.com>
2020-06-20 11:46:50 +0530

    Initial Commit

---
+++ Slide Cards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slide Cards
+//
+//  Created by Gokul Nair on 20/06/20.
+//  Copyright © 2020 Gokul Nair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
